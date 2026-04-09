b05bfbc1cae586e2d8e3e71760b903e630a355b6
Bhanu🧑‍💻 <bhanu99517@gmail.com>
2026-04-09 09:16:55 +0530

    Remove licensing request comment
    Removed licensing request comment from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
 // © 2026 Bhanu Prakash - Unauthorized use prohibited
-// For licensing requests: bhanu99517@gmail.com
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
