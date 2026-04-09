f0973f602b71da6aef1e47420a82e87d59fa8fe6
Bhanu🧑‍💻 <bhanu99517@gmail.com>
2026-04-09 09:16:39 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 // © 2026 Bhanu Prakash - Unauthorized use prohibited
+// For licensing requests: bhanu99517@gmail.com
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
